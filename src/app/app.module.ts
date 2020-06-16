31406bee8060a801052e5cdd4d9c07aba2d63070
Mandar <Mdesh1@ocdsb.ca>
2020-06-16 15:05:44 -0400

    Full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+
+import { GridsterModule } from 'angular-gridster2';
+import { Example1Component } from './components/example1/example1.component';
+import { Example2Component } from './components/example2/example2.component';
+import { LayoutItemDirective } from './directives/layout-item.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    Example1Component,
+    Example2Component,
+    LayoutItemDirective,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, GridsterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [Example1Component, Example2Component],
 })
-export class AppModule { }
+export class AppModule {}
